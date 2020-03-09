99d3a0c2f17709fb1dde15659af6ab21466d67a4
Rodrigo Costa <rodrigojcosta7@gmail.com>
2020-03-09 13:10:16 -0300

    Ajustes no layout

--- src/pages/Aulas.jsx
+++ src/pages/Aulas.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Button } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Button, ScrollView } from 'react-native';
 import { Content, Card, CardItem, Body, Right } from 'native-base';
 import { FontAwesome } from '@expo/vector-icons';
 import db from '../../db.json';
@@ -18,55 +18,111 @@ function groupBy(list, keyGetter) {
   return map;
 }
 export default class Aulas extends React.Component {
-  aulas = [];
   constructor(props) {
     super(props);
     this.state = {
-      aulas: [],
-      turnoManha: [],
-      turnoVerspertino: []
+      aulas: []
     };
   }
-  getListTurno(list, turno) {
-    console.log(list);
 
+  getDataName(index) {
+    var days = ['Segunda Feira', 'Terça Feira', 'Quarta Feira', 'Quinta Feira', 'Sexta Feira'];
+    return days[index]
+  }
+
+  getAulas(turno) {
+    return turno.map(aula => {
+      return <View style={{
+        flex: 1,
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        alignItems: 'flex-start',
+        alignContent: 'center',
+        justifyContent: 'center'
+      }}>
+        <TouchableOpacity style={{
+          backgroundColor: 'blue', width: 50, height: 40, flexDirection: 'row',
+          alignItems: 'center',
+          margin: 5,
+          minHeight: 20,
+          borderRadius: 3
+        }}>
+          <Text style={{ color: 'white', textAlign: 'center' }}> {aula.siglaEscola} </Text>
+        </TouchableOpacity>
+      </View>
+    })
+  }
+
+  getListTurno(list) {
     var turnos = groupBy(list, tur => tur.turno);
     var turnoManha = turnos.get('M');
     var turnotarde = turnos.get('V');
+    console.log(turnoManha)
+    console.log(turnotarde)
     if (turnoManha != undefined || turnotarde !== undefined) {
       return <View>
         {turnoManha !== undefined ? <Text style={{ textAlign: 'center', color: 'black', padding: 5 }} >
-          Segunda feira -  {turnoManha[0].dataSemana} - {turno}
+          {this.getDataName(list[0].diaSemana - 1)} -  {turnoManha[0].dataSemana} - Manhã
         </Text> : null}
-        {turnotarde !== undefined ? <Text style={{ textAlign: 'center', color: 'black', padding: 5 }} >
-          Segunda feira -  {turnoManha[0].dataSemana} - 'tarde'
-        </Text> : null}
-      </View>
-
-
+        <View style={{
+          flex: 1,
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+          alignItems: 'flex-start',
+          alignContent: 'center',
+          justifyContent: 'center'
+        }}>
+          {turnoManha !== undefined ? turnoManha.map(aula => {
+            return <TouchableOpacity key={aula.id} style={{
+              backgroundColor: 'blue', width: 50, height: 40, flexDirection: 'row',
+              alignItems: 'center',
+              margin: 5,
+              minHeight: 20,
+              borderRadius: 3
+            }}>
+              <Text style={{ color: 'white', textAlign: 'center' }}> {aula.siglaEscola} </Text>
+            </TouchableOpacity>
+          }) : null}
+        </View>
+        {
+          turnotarde !== undefined ? <Text style={{ textAlign: 'center', color: 'black', padding: 5 }} >
+            {this.getDataName(list[0].diaSemana - 1)}  -  {turnotarde[0].dataSemana} - Tarde
+        </Text> : null
+        }
+        <View style={{
+          flex: 1,
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+          alignItems: 'flex-start',
+          alignContent: 'center',
+          justifyContent: 'center',
+        }}>
+          {turnotarde !== undefined ? turnotarde.map(aula => {
+            return <TouchableOpacity key={aula.id} style={{
+              backgroundColor: 'blue', width: 50, height: 40, flexDirection: 'row',
+              alignItems: 'center',
+              margin: 5,
+              minHeight: 20,
+              borderRadius: 3
+            }}>
+              <Text style={{ color: 'white', textAlign: 'center' }}> {aula.siglaEscola} </Text>
+            </TouchableOpacity>
+          }) : null}
+        </View>
+      </View >
     }
     return null
+  }
+
+  getAulas() {
+
   }
   async componentDidMount() {
     const dataGroup = groupBy(db._embedded.aulas, aula => aula.dataSemana);
     const res = [...dataGroup]
-    console.log(res)
-    var turnos;
-    var turnoManha;
-    var turnoVerspertino;
-    res.forEach(x => {
-      turnos = groupBy(x[1], tur => tur.turno);
-      console.log(turnos)
-      turnoManha = turnos.get('M');
-      console.log(turnoManha);
-      turnoVerspertino = turnos.get('V');
-    })
-
     console.log(res.length)
     this.setState({
-      aulas: res,
-      turnoManha,
-      turnoVerspertino
+      aulas: res
     });
 
   }
@@ -96,16 +152,17 @@ export default class Aulas extends React.Component {
             shadowOpacity: 0.25,
             shadowRadius: 3.84,
             elevation: 5,
+            overflow: 'hidden'
           }} >
             <View style={{
               height: 40,
-              borderTopLeftRadius: 15,
-              borderTopRightRadius: 15,
-              borderColor: '#d2d1d1',
               paddingTop: 5,
-              backgroundColor: '#d2d1d1',
+              backgroundColor: '#d2d1d1'
             }}>
               <View style={{
+                display: 'flex',
+                flexWrap: 'wrap',
+                alignItems: 'center',
                 flexDirection: "row",
                 justifyContent: "center",
                 alignItems: "center"
@@ -115,23 +172,22 @@ export default class Aulas extends React.Component {
                 <FontAwesome name="calendar"
                   color="#000"
                   size={25}
-                  style={{ marginHorizontal: 5 }}
+                  style={{ marginHorizontal: 5, float: 'right' }}
                 />
               </View>
             </View>
-            <View >
-
+            <ScrollView >
               {this.state.aulas.length > 0 ? this.state.aulas.map(x => {
-                return this.getListTurno(x[1], 'M')
+                return this.getListTurno(x[1])
               }) : null}
-{/* 
+              {/* 
               {this.state.aulas.length > 0 ? this.state.aulas.map(x => {
                 return this.getListTurno(x[1], 'V')
               }) : null} */}
               {/* <Text style={{ textAlign: 'center', color: 'black', padding: 5 }} >
                 Segunda feira -  10/03/2020 - Manha
             </Text> */}
-              <View style={{
+              {/* <View style={{
                 flex: 1,
                 flexDirection: 'row',
                 flexWrap: 'wrap',
@@ -155,8 +211,8 @@ export default class Aulas extends React.Component {
                   borderBottomColor: 'black',
                   borderBottomWidth: 1,
                 }}
-              />
-            </View>
+              /> */}
+            </ScrollView>
           </View>
         </View>
       </View>
